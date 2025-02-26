d8c71869541193992997c459163c90ffde03a79e
Christina Ying Wang <christina@balena.io>
2025-02-26 12:46:41 -0800

    Add fixtures for testing
    Signed-off-by: Christina Ying Wang <christina@balena.io>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    include: ["test/**/*.test.ts"],
-    coverage: {
-      provider: "v8",
-    },
-  },
+	test: {
+		include: ['test/**/*.test.ts'],
+		coverage: {
+			provider: 'v8',
+		},
+		testTimeout: 30000,
+		watch: false,
+	},
 });
